5879255fe0f17f70b7b2b6da1cc8ed94399bc76e
Guilherme <guisam.halter@gmail.com>
2023-05-19 08:47:03 -0300

    feito projeto garagem certinho

---
+++ Garagem/views.py
@@ -0,0 +1,33 @@
+from rest_framework.viewsets import ModelViewSet
+
+from Garagem.models import Acessorio, Categoria, Cor, Marca, Veiculo
+from Garagem.serializers import AcessorioSerializer, CategoriaSerializer, CorSerializer, MarcaSerializer, VeiculoSerializer, VeiculoDetailSerializer, VeiculoListSerializer
+
+class AcessorioViewSet(ModelViewSet):
+    queryset = Acessorio.objects.all()
+    serializer_class = AcessorioSerializer
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer   
+
+
+class CorViewSet(ModelViewSet):
+    queryset = Cor.objects.all()
+    serializer_class = CorSerializer        
+
+class MarcaViewSet(ModelViewSet):
+    queryset = Marca.objects.all()
+    serializer_class = MarcaSerializer
+
+class VeiculoViewSet(ModelViewSet):
+    queryset = Veiculo.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return VeiculoListSerializer
+        elif self.action == "retrieve":
+            return VeiculoDetailSerializer
+        return VeiculoSerializer
+
+# Create your views here.
